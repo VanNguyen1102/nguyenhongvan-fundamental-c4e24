8e2191e068a036373bd457822bafc35113cb1215
VanNguyen1102 <vanb1192@gmail.com>
2019-01-07 22:50:46 +0700

    update

--- Web2-Homework/serious1.py
+++ Web2-Homework/serious1.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/register")
 def register():
     return render_template("register.html")
+
+@app.route("/database", methods=['GET', 'POST'])
+def database():
+    if request.method == 'POST':
+        database = request.form
+        return render_template("database.html", database=database)
 
 if __name__ == "__main__":
     app.run(debug=True)
